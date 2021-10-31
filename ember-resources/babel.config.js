49462ede3dd03fd1ff0e4d33d680d7fd79e4be8f
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-30 20:51:08 -0400

    chore(internal): figure out how to compile a v2 addon

---
+++ ember-resources/babel.config.js
@@ -0,0 +1,17 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const resolve = require.resolve;
+
+export default {
+  plugins: [
+    [resolve('@babel/plugin-transform-typescript'), {
+        allowDeclareFields: true,
+        onlyRemoveTypeImports: true,
+        optimizeConstEnums: true,
+    }],
+    [resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+    resolve('@babel/plugin-proposal-class-properties'),
+    resolve('@embroider/addon-dev/template-colocation-plugin'),
+  ],
+};
