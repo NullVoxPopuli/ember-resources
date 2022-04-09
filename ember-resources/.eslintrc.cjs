65aa4712bfcace7662639c7fdb1049dfd8aa8173
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-09 09:06:41 -0400

    chore(internal): infra, build updates

--- ember-resources/.eslintrc.cjs
+++ ember-resources/.eslintrc.cjs
@@ -6,5 +6,30 @@ const config = configs.ember();
 
 module.exports = {
   ...config,
-  overrides: [...config.overrides],
+  overrides: [
+    ...config.overrides,
+    {
+      files: ['**/*.ts'],
+      rules: {
+        /**
+         * any can be useful
+         */
+        '@typescript-eslint/no-explicit-any': 'off',
+        /**
+         * there is heavy use of `object` in this library
+         */
+        '@typescript-eslint/ban-types': 'off',
+        /**
+         * The following types do are not defined by the definitely typed packages
+         * - @glimmer/tracking/primitives/cache
+         *   - getValue
+         * - @ember/helper
+         *   - invokeHelper
+         *   - capabilities
+         *   - setHelperManager
+         */
+        '@typescript-eslint/ban-ts-comment': 'off',
+      },
+    },
+  ],
 };
