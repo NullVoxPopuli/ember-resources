4cce0c53d0478f2db6c868275eb036469da1bf84
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-09 10:40:52 -0400

    feat(docs, types): fix types with the map resource. update docs

--- ember-resources/src/util/map.ts
+++ ember-resources/src/util/map.ts
@@ -4,15 +4,152 @@ import { assert } from '@ember/debug';
 import { Resource } from '../core';
 
 /**
+ * Public API of the return value of the [[map]] resource.
+ *
+ * This is proxied, so you can still do array-index-style access.
+ *
+ * ```js
+ *  class Foo {
+ *    myMappedData = map(this, {
+ *      data: () => [1, 2, 3],
+ *      map: (num) => `hi, ${num}!`
+ *    });
+ *
+ *    get first() {
+ *      return this.myMappedData[0];
+ *    }
+ *  }
+ * ```
+ *
+ * Iteration is also possible:
+ * ```js
+ *  class Foo {
+ *    myMappedData = map(this, {
+ *      data: () => [1, 2, 3],
+ *      map: (num) => `hi, ${num}!`
+ *    });
+ *
+ *    get mapAgain() {
+ *      let results = [];
+ *
+ *      for (let datum of this.myMappedData) {
+ *        results.push(datum);
+ *      }
+ *
+ *      return datum;
+ *    }
+ *  }
+ * ```
+ */
+export interface MappedArray<MappedTo> {
+  /**
+   * evaluate and return an array of all mapped items.
+   *
+   * This is useful when you need to do other Array-like operations
+   * on the mapped data, such as filter, or find
+   *
+   * ```js
+   *  class Foo {
+   *    myMappedData = map(this, {
+   *      data: () => [1, 2, 3],
+   *      map: (num) => `hi, ${num}!`
+   *    });
+   *
+   *    get everything() {
+   *      return this.myMappedData.values();
+   *    }
+   *  }
+   * ```
+   */
+  values: () => MappedTo[];
+
+  /**
+   * Without evaluating the map function on each element,
+   * provide the total number of elements
+   *
+   * ```js
+   *  class Foo {
+   *    myMappedData = map(this, {
+   *      data: () => [1, 2, 3],
+   *      map: (num) => `hi, ${num}!`
+   *    });
+   *
+   *    get numItems() {
+   *      return this.myMappedData.length;
+   *    }
+   *  }
+   * ```
+   */
+  get length(): number;
+
+  /**
+   * Iterate over the mapped array, lazily invoking the passed map function
+   * that was passed to [[map]].
+   *
+   * This will always return previously mapped records without re-evaluting
+   * the map function, so the default `{{#each}}` behavior in ember will
+   * be optimized on "object-identity". e.g.:
+   *
+   * ```js
+   *  // ...
+   *  myMappedData = map(this, {
+   *    data: () => [1, 2, 3],
+   *    map: (num) => `hi, ${num}!`
+   *  });
+   *  // ...
+   * ```
+   * ```hbs
+   *  {{#each this.myMappedData as |datum|}}
+   *     loop body only invoked for changed entries
+   *     {{datum}}
+   *  {{/each}}
+   * ```
+   *
+   * Iteration in javascript is also provided by this iterator
+   * ```js
+   *  class Foo {
+   *    myMappedData = map(this, {
+   *      data: () => [1, 2, 3],
+   *      map: (num) => `hi, ${num}!`
+   *    });
+   *
+   *    get mapAgain() {
+   *      let results = [];
+   *
+   *      for (let datum of this.myMappedData) {
+   *        results.push(datum);
+   *      }
+   *
+   *      return datum;
+   *    }
+   *  }
+   * ```
+   */
+  [Symbol.iterator](): Iterator<MappedTo>;
+}
+
+/**
  * Reactivily apply a `map` function to each element in an array,
  * persisting map-results for each object, based on identity.
  *
- * @param {object} [destroyable] parent destroyable context, usually `this`
- * @param {object} [options] object specifying the map function and the data to use
- * @param {(element: Element) => MapTo} [options.map] the map function
- * @param {() => Element[]} [options.data] a thunk that returns the array to map over. This should access tracked data so that the Resources knows when to update.
+ * This is useful when you have a large collection of items that
+ * need to be transformed into a different shape (adding/removing/modifying data/properties)
+ * and you want the transform to be efficient when iterating over that data.
  *
- * @return {Proxy<TrackedArrayMap>} an object that behaves like an array. This shouldn't be modified directly. Instead, you can freely modify the data returned by the `data` function, which should be tracked in order to benefit from this abstraction.
+ * A common use case where this `map` resource provides benefits over is
+ * ```js
+ * class MyClass {\
+ *   @cached
+ *   get wrappedRecords() {
+ *     return this.records.map(record => new SomeWrapper(record));
+ *   }
+ * }
+ * ```
+ *
+ * Even though the above is `@cached`, if any tracked data accessed during the evaluation of `wrappedRecords`
+ * changes, the entire array.map will re-run, often doing duplicate work for every unchanged item in the array.
+ *
+ * @return {MappedArray} an object that behaves like an array. This shouldn't be modified directly. Instead, you can freely modify the data returned by the `data` function, which should be tracked in order to benefit from this abstraction.
  *
  * @example
  *
@@ -28,9 +165,25 @@ import { Resource } from '../core';
  * ```
  */
 export function map<Element = unknown, MapTo = unknown>(
+  /**
+   * parent destroyable context, unually `this`
+   */
   destroyable: object,
   options: {
+    /**
+     * Array of non-primitives to map over
+     *
+     * This can be class instances, plain objects, or anything supported by WeakMap's key
+     */
     data: () => Element[];
+    /**
+     * How to transform each element from `data`,
+     * similar to if you were to use Array map yourself.
+     *
+     * This function will be called only when needed / on-demand / lazily.
+     * - if iterating over part of the data, map will only be called for the elements observed
+     * - if not iterating, map will only be called for the elements observed.
+     */
     map: (element: Element) => MapTo;
   }
 ) {
@@ -66,9 +219,7 @@ export function map<Element = unknown, MapTo = unknown>(
       return Reflect.get(target, property, receiver);
     },
     // Is there a way to do this without lying to TypeScript?
-  }) as unknown as TrackedArrayMap<Element, MapTo> & {
-    [key: number]: MapTo;
-  } & Iterable<MapTo>;
+  }) as unknown as MappedArray<MapTo>;
 }
 
 type PositionalArgs<E = unknown> = [E[]];
@@ -78,10 +229,16 @@ interface NamedArgs<E = unknown, Result = unknown> {
 
 const AT = Symbol('__AT__');
 
-export class TrackedArrayMap<Element = unknown, MappedTo = unknown> extends Resource<{
-  positional: PositionalArgs<Element>;
-  named: NamedArgs<Element, MappedTo>;
-}> {
+/**
+ * @private
+ */
+export class TrackedArrayMap<Element = unknown, MappedTo = unknown>
+  extends Resource<{
+    positional: PositionalArgs<Element>;
+    named: NamedArgs<Element, MappedTo>;
+  }>
+  implements MappedArray<MappedTo>
+{
   #map = new WeakMap<Element & object, MappedTo>();
 
   @tracked private declare _records: (Element & object)[];
@@ -96,16 +253,31 @@ export class TrackedArrayMap<Element = unknown, MappedTo = unknown> extends Reso
     this._map = map;
   }
 
-  /**
-   * @public
-   *
-   * evaluate and return an array of all mapped items
-   */
   values = () => [...this];
 
-  // @public
   get length() {
     return this._records.length;
+  }
+
+  [Symbol.iterator](): Iterator<MappedTo> {
+    let i = 0;
+
+    return {
+      next: () => {
+        if (i >= this.length) {
+          return { done: true, value: null };
+        }
+
+        let value = this[AT](i);
+
+        i++;
+
+        return {
+          value,
+          done: false,
+        };
+      },
+    };
   }
 
   /**
@@ -124,27 +296,5 @@ export class TrackedArrayMap<Element = unknown, MappedTo = unknown> extends Reso
     }
 
     return value;
-  }
-
-  // @public
-  [Symbol.iterator]() {
-    let i = 0;
-
-    return {
-      next: () => {
-        if (i >= this.length) {
-          return { done: true };
-        }
-
-        let value = this[AT](i);
-
-        i++;
-
-        return {
-          value,
-          done: false,
-        };
-      },
-    };
   }
 }
