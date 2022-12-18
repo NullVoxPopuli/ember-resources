2efd5d8db1865e084c1aa7dad5ef0bbbc04049c1
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-12-18 15:10:28 -0500

    Might have an issue with mts

--- eslint-plugin/src/rules/no-this-property-assignment-in-tracked-function.js
+++ eslint-plugin/src/rules/no-this-property-assignment-in-tracked-function.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { localName } = require('../utils');
+
 /** @type {import('eslint').Rule.RuleModule} */
 module.exports = {
   meta: {
@@ -7,5 +9,39 @@ module.exports = {
       recommended: true,
       category: 'side-effects',
     },
+    messages: {
+      noSideEffects:
+        'Resources are meant to be encapsulated values with cleanup. Side-effects are not allowed.',
+    },
+  },
+  create(context) {
+    let trackedFunctionName;
+    let lastCallStack = [];
+
+    return {
+      ...localName(
+        'ember-resources/util/function',
+        'trackedFunction',
+        (name) => (trackedFunctionName = name)
+      ),
+      CallExpression(node) {
+        if (node.callee.name !== trackedFunctionName) return;
+
+        lastCallStack.push(node);
+      },
+      'CallExpression:exit'(node) {
+        if (node.callee.name !== trackedFunctionName) return;
+
+        lastCallStack.pop();
+      },
+      'AssignmentExpression[left.object.type="ThisExpression]'(node) {
+        if (lastCallStack.length > 0) {
+          context.report({
+            node: node,
+            message: 'noSideEffects',
+          });
+        }
+      },
+    };
   },
 };
--- eslint-plugin/tests/rules/no-this-property-assignment-in-tracked-function.mts
+++ eslint-plugin/tests/rules/no-this-property-assignment-in-tracked-function.mts
@@ -1,10 +1,29 @@
-'use strict';
+import { basename, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-/** @type {import('eslint').Rule.RuleModule} */
-module.exports = {
-  meta: {
-    docs: {
-      recommended: true,
-    },
-  },
-};
+import { describe, it, expect } from 'vitest';
+import { RuleTester } from 'eslint';
+
+import rule from '../../src/rules/no-this-property-assignment-in-tracked-function.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const filename = basename(__filename);
+
+let ruleTester = new RuleTester();
+
+
+describe(filename, () => {
+  ruleTester.run('no-this-property-assignment-in-tracked-function', rule, {
+    valid: [
+      `import { trackedFunction } from 'ember-resources/util/function';\n` 
+      + `class Foo {\n`
+      + '  foo = trackedFunction(this, () => {\n'
+      + '    this.bar = 2;'                           
+      + '  })\n'
+      + `}`],
+    invalid: [
+
+    ],
+  })
+});
+
