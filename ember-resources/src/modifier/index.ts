bb4ededfd641730e2b5742125337ab520a4accda
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-20 10:31:31 -0700

    lint:fix

--- ember-resources/src/modifier/index.ts
+++ ember-resources/src/modifier/index.ts
@@ -25,6 +25,44 @@ type ArgsForFn<S> = S extends { Args?: object }
     : [...PositionalArgs<S>, NamedArgs<S>]
   : [];
 
+/**
+ * A resource-based API for building modifiers.
+ *
+ * You can attach this to an element, and use a `resource` to manage
+ * the state, add event listeners, remove event listeners on cleanup, etc.
+ *
+ * Using resources for modifiers provides a clear and concise API with
+ * easy to read concerns.
+   *
+   *
+* The signature for the modifier here is _different_ from `ember-modifier`, where positional args and named args are grouped together into an array and object respectively.
+
+* This signature for ember-resource's `modifier` follows the [plain function invocation](https://blog.emberjs.com/plain-old-functions-as-helpers/) signature.
+ *
+* ```js
+* import { resource } from 'ember-resources';
+* import { modifier } from 'ember-resources/modifier';
+*
+* const wiggle = modifier((element, arg1, arg2, namedArgs) => {
+*     return resource(({ on }) => {
+*         let animation = element.animate([
+*             { transform: `translateX(${arg1}px)` },
+*             { transform: `translateX(-${arg2}px)` },
+*         ], {
+*             duration: 100,
+*             iterations: Infinity,
+*         });
+*
+*         on.cleanup(() => animation.cancel());
+*     });
+* });
+*
+* <template>
+*     <div {{wiggle 2 5 named="hello"}}>hello</div>
+* </template>
+```
+ *
+ */
 export function modifier<El extends Element, Args extends unknown[] = unknown[]>(
   fn: (element: El, ...args: Args) => void
 ): ModifierLike<{
