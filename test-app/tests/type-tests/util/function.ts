e50d1fb71082dee06e25e6e03b8ab3d47bd8c4c6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-08 18:40:11 -0500

    Drop all util files

--- test-app/tests/type-tests/util/function.ts
+++
@@ -1,7 +0,0 @@
-import { trackedFunction } from 'ember-resources/util/function';
-import { expectType } from 'ts-expect';
-
-import type { State } from 'ember-resources/util/function';
-
-expectType<State<number>>(trackedFunction(globalThis, () => 2));
-expectType<State<{ foo: boolean }>>(trackedFunction(globalThis, () => ({ foo: true })));
