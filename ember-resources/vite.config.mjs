54aeb3d102c610638fc72bf2a3dd4be5e689ba77
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-10 15:26:22 -0500

    Can't lint:types because we aren't type=module

--- ember-resources/vite.config.mjs
+++ ember-resources/vite.config.mjs
@@ -41,18 +41,23 @@ export default defineConfig({
       outDir: 'declarations',
     }),
     {
-      /**
-       * Related issues
-       * - https://github.com/embroider-build/embroider/issues/1672
-       * - https://github.com/embroider-build/embroider/pull/1572
-       * - https://github.com/embroider-build/embroider/issues/1675
-       *
-       * Fixed in embroider@4 and especially @embroider/vite
-       */
       name: 'use-weird-non-ESM-ember-convention',
       closeBundle: async () => {
+        /**
+         * Related issues
+         * - https://github.com/embroider-build/embroider/issues/1672
+         * - https://github.com/embroider-build/embroider/pull/1572
+         * - https://github.com/embroider-build/embroider/issues/1675
+         *
+         * Fixed in embroider@4 and especially @embroider/vite
+         */
         await execaCommand('cp dist/index.mjs dist/index.js', { stdio: 'inherit' });
         console.log('⚠️ Incorrectly (but neededly) renamed MJS module to JS in a CJS package');
+
+        /**
+         * https://github.com/microsoft/TypeScript/issues/56571#
+         * README: https://github.com/NullVoxPopuli/fix-bad-declaration-output
+         */
         await execaCommand(`pnpm fix-bad-declaration-output declarations/`, {
           stdio: 'inherit',
         });
