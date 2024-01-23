486020f6034a61af2a4c6882f60a391031df4c02
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-23 17:35:16 -0500

    Try to test out https://github.com/embroider-build/embroider/pull/1766 blocked by https://github.com/embroider-build/embroider/pull/1766#issuecomment-1907034602

--- ember-resources/vite.config.mjs
+++ ember-resources/vite.config.mjs
@@ -1,9 +1,13 @@
 import { resolve } from 'node:path';
 import url from 'node:url';
 
+import { Addon } from '@embroider/addon-dev/rollup';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { execaCommand } from 'execa';
+import { fixBadDeclarationOutput } from 'fix-bad-declaration-output';
+
+const addon = new Addon();
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -50,29 +54,6 @@ export default defineConfig({
       rollupTypes: true,
       outDir: 'declarations',
     }),
-    {
-      name: 'use-weird-non-ESM-ember-convention',
-      closeBundle: async () => {
-        /**
-         * Related issues
-         * - https://github.com/embroider-build/embroider/issues/1672
-         * - https://github.com/embroider-build/embroider/pull/1572
-         * - https://github.com/embroider-build/embroider/issues/1675
-         *
-         * Fixed in embroider@4 and especially @embroider/vite
-         */
-        await execaCommand('cp dist/index.mjs dist/index.js', { stdio: 'inherit' });
-        console.log('⚠️ Incorrectly (but neededly) renamed MJS module to JS in a CJS package');
-
-        /**
-         * https://github.com/microsoft/TypeScript/issues/56571#
-         * README: https://github.com/NullVoxPopuli/fix-bad-declaration-output
-         */
-        await execaCommand(`pnpm fix-bad-declaration-output declarations/`, {
-          stdio: 'inherit',
-        });
-        console.log('⚠️ Dangerously (but neededly) fixed bad declaration output from typescript');
-      },
-    },
+    addon.glint('declarations/**/*.d.ts'),
   ],
 });
