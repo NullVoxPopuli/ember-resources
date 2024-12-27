a11d11194ce5237e4b6f422775e089cc36e96456
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-12-27 15:13:29 -0500

    Add testing for ember-source 5.8 and 5.12

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -58,6 +58,22 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-5.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-5.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
