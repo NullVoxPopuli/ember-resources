57fc376c8df3ecb7fe0234aa56a65dea54d4217d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-01 19:23:30 -0500

    Lints

--- test-app/app/app.ts
+++ test-app/app/app.ts
@@ -1,10 +1,9 @@
 import Application from '@ember/application';
 
+import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 import config from 'test-app/config/environment';
-
-import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 
 setupDeprecationWorkflow({
   throwOnUnhandled: true,
