647d7a511c721cffc89363816fd8957cd62522f9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-06-20 15:12:11 -0400

    Repro: #1195

--- codemod/src/v6-to-v7/imports.changeNamedImport.test.ts
+++ codemod/src/v6-to-v7/imports.changeNamedImport.test.ts
@@ -169,4 +169,90 @@ describe('Resource', () => {
           "
     `);
   });
+
+  /**
+   * https://github.com/NullVoxPopuli/ember-resources/issues/1195#issuecomment-2992470578
+   *
+   * Repro for #1195
+   */
+  test(`existing imports that aren't relevant remain intact`, () => {
+    let text = `
+import * as emberComponent from '@ember/component';
+import * as emberComponentTemplateOnly from '@ember/component/template-only';
+import * as emberDestroyable from '@ember/destroyable';
+import * as emberHelper from '@ember/helper';
+import * as emberModifier from '@ember/modifier';
+import * as emberObject from '@ember/object';
+import * as emberObjectInternals from '@ember/object/internals';
+import * as emberTemplate from '@ember/template';
+import * as emberTemplateFactory from '@ember/template-factory';
+import * as glimmerComponent from '@glimmer/component';
+import * as glimmerTracking from '@glimmer/tracking';
+
+import * as dateFns from 'date-fns';
+import * as emberConcurrency from 'ember-concurrency';
+import * as emberConcurrencyAsyncArrowRuntime from 'ember-concurrency/-private/async-arrow-runtime';
+import * as cssUrl from 'ember-css-url';
+import * as emberModifier2 from 'ember-modifier';
+import * as emberModifyClassBasedResource from 'ember-modify-based-class-resource';
+import * as emberProvideConsumeContext from 'ember-provide-consume-context';
+import * as emberProvideConsumeContextContextConsumer from 'ember-provide-consume-context/components/context-consumer';
+import * as emberProvideConsumeContextContextProvider from 'ember-provide-consume-context/components/context-provider';
+import * as emberResources from 'ember-resources';
+import * as flat from 'flat';
+import * as lodash from 'lodash';
+import * as matrixJsSDK from 'matrix-js-sdk';
+import * as superFastMD5 from 'super-fast-md5';
+import * as tracked from 'tracked-built-ins';
+
+import * as boxelUiComponents from '@cardstack/boxel-ui/components';
+import * as boxelUiHelpers from '@cardstack/boxel-ui/helpers';
+import * as boxelUiIcons from '@cardstack/boxel-ui/icons';
+import * as boxelUiModifiers from '@cardstack/boxel-ui/modifiers';
+
+import * as runtime from '@cardstack/runtime-common';
+import { VirtualNetwork } from '@cardstack/runtime-common';
+`;
+    let result = doIt(text);
+
+    expect(result).toMatchInlineSnapshot(`
+      "
+      import * as emberComponent from '@ember/component';
+      import * as emberComponentTemplateOnly from '@ember/component/template-only';
+      import * as emberDestroyable from '@ember/destroyable';
+      import * as emberHelper from '@ember/helper';
+      import * as emberModifier from '@ember/modifier';
+      import * as emberObject from '@ember/object';
+      import * as emberObjectInternals from '@ember/object/internals';
+      import * as emberTemplate from '@ember/template';
+      import * as emberTemplateFactory from '@ember/template-factory';
+      import * as glimmerComponent from '@glimmer/component';
+      import * as glimmerTracking from '@glimmer/tracking';
+
+      import * as dateFns from 'date-fns';
+      import * as emberConcurrency from 'ember-concurrency';
+      import * as emberConcurrencyAsyncArrowRuntime from 'ember-concurrency/-private/async-arrow-runtime';
+      import * as cssUrl from 'ember-css-url';
+      import * as emberModifier2 from 'ember-modifier';
+      import * as emberModifyClassBasedResource from 'ember-modify-based-class-resource';
+      import * as emberProvideConsumeContext from 'ember-provide-consume-context';
+      import * as emberProvideConsumeContextContextConsumer from 'ember-provide-consume-context/components/context-consumer';
+      import * as emberProvideConsumeContextContextProvider from 'ember-provide-consume-context/components/context-provider';
+      import * as emberResources from 'ember-resources';
+      import * as flat from 'flat';
+      import * as lodash from 'lodash';
+      import * as matrixJsSDK from 'matrix-js-sdk';
+      import * as superFastMD5 from 'super-fast-md5';
+      import * as tracked from 'tracked-built-ins';
+
+      import * as boxelUiComponents from '@cardstack/boxel-ui/components';
+      import * as boxelUiHelpers from '@cardstack/boxel-ui/helpers';
+      import * as boxelUiIcons from '@cardstack/boxel-ui/icons';
+      import * as boxelUiModifiers from '@cardstack/boxel-ui/modifiers';
+
+      import * as runtime from '@cardstack/runtime-common';
+      import { VirtualNetwork } from '@cardstack/runtime-common';
+      "
+    `);
+  });
 });
